8c92f59775e9af966347090fb636674e6c9f33a8
Brian Michel <brian.michel@gmail.com>
2023-09-18 12:48:31 -0400

    Remove unused comments

--- Sources/ComposableArchitecture/Effect.swift
+++ Sources/ComposableArchitecture/Effect.swift
@@ -6,40 +6,59 @@ import Combine
 import Foundation
 import XCTestDynamicOverlay
 
-/// A type that encapsulates a unit of work that can be run in the outside world, and can feed
-/// actions back to the ``Store``.
-///
-/// Effects are the perfect place to do side effects, such as network requests, saving/loading
-/// from disk, creating timers, interacting with dependencies, and more. They are returned from
-/// reducers so that the ``Store`` can perform the effects after the reducer is done running.
-///
-/// There are 2 distinct ways to create an `Effect`: one using Swift's native concurrency tools, and
-/// the other using Apple's Combine framework:
-///
-/// * If using Swift's native structured concurrency tools then there are 3 main ways to create an
-/// effect, depending on if you want to emit one single action back into the system, or any number
-/// of actions, or just execute some work without emitting any actions:
-///   * ``EffectPublisher/task(priority:operation:catch:file:fileID:line:)``
-///   * ``EffectPublisher/run(priority:operation:catch:file:fileID:line:)``
-///   * ``EffectPublisher/fireAndForget(priority:_:)``
-/// * If using Combine in your application, in particular for the dependencies of your feature
-/// then you can create effects by making use of any of Combine's operators, and then erasing the
-/// publisher type to ``EffectPublisher`` with either `eraseToEffect` or `catchToEffect`. Note that
-/// the Combine interface to ``EffectPublisher`` is considered soft deprecated, and you should
-/// eventually port to Swift's native concurrency tools.
-///
-/// > Important: ``Store`` is not thread safe, and so all effects must receive values on the same
-/// thread. This is typically the main thread,  **and** if the store is being used to drive UI then
-/// it must receive values on the main thread.
-/// >
-/// > This is only an issue if using the Combine interface of ``EffectPublisher`` as mentioned
-/// above. If  you are using Swift's concurrency tools and the `.task`, `.run` and `.fireAndForget`
-/// functions on ``EffectTask``, then threading is automatically handled for you.
-
 #if canImport(SwiftUI)
 import SwiftUI
 #endif
 
+/// This type is deprecated in favor of ``EffectTask``. See its documentation for more information.
+@available(
+  iOS,
+  deprecated: 9999.0,
+  message:
+    """
+    'EffectPublisher' has been deprecated in favor of 'EffectTask'.
+
+     You are encouraged to use `EffectTask<Action>` to model the output of your reducers, and to use Swift concurrency to model asynchrony in dependencies.
+
+     See the migration roadmap for more information: https://github.com/pointfreeco/swift-composable-architecture/discussions/1477
+    """
+)
+@available(
+  macOS,
+  deprecated: 9999.0,
+  message:
+    """
+    'EffectPublisher' has been deprecated in favor of 'EffectTask'.
+
+     You are encouraged to use `EffectTask<Action>` to model the output of your reducers, and to use Swift concurrency to model asynchrony in dependencies.
+
+     See the migration roadmap for more information: https://github.com/pointfreeco/swift-composable-architecture/discussions/1477
+    """
+)
+@available(
+  tvOS,
+  deprecated: 9999.0,
+  message:
+    """
+    'EffectPublisher' has been deprecated in favor of 'EffectTask'.
+
+     You are encouraged to use `EffectTask<Action>` to model the output of your reducers, and to use Swift concurrency to model asynchrony in dependencies.
+
+     See the migration roadmap for more information: https://github.com/pointfreeco/swift-composable-architecture/discussions/1477
+    """
+)
+@available(
+  watchOS,
+  deprecated: 9999.0,
+  message:
+    """
+    'EffectPublisher' has been deprecated in favor of 'EffectTask'.
+
+     You are encouraged to use `EffectTask<Action>` to model the output of your reducers, and to use Swift concurrency to model asynchrony in dependencies.
+
+     See the migration roadmap for more information: https://github.com/pointfreeco/swift-composable-architecture/discussions/1477
+    """
+)
 public struct EffectPublisher<Action, Failure: Error> {
   @usableFromInline
   enum Operation {
@@ -402,55 +421,7 @@ public struct Send<Action> {
   #endif
 }
 
-// MARK: - Creating Effects
-
-/// A convenience type alias for referring to an effect that can never fail, like the kind of
-/// ``EffectPublisher`` returned by a reducer after processing an action.
-///
-/// Instead of specifying `Never` as `Failure`:
-///
-/// ```swift
-/// func reduce(into state: inout State, action: Action) -> EffectPublisher<Action, Never> { … }
-/// ```
-///
-/// You can specify a single generic:
-///
-/// ```swift
-/// func reduce(into state: inout State, action: Action) -> EffectTask<Action>  { … }
-/// ```
-
-/// A type that can send actions back into the system when used from
-/// ``EffectPublisher/run(priority:operation:catch:file:fileID:line:)``.
-///
-/// This type implements [`callAsFunction`][callAsFunction] so that you invoke it as a function
-/// rather than calling methods on it:
-///
-/// ```swift
-/// return .run { send in
-///   send(.started)
-///   defer { send(.finished) }
-///   for await event in self.events {
-///     send(.event(event))
-///   }
-/// }
-/// ```
-///
-/// You can also send actions with animation:
-///
-/// ```swift
-/// send(.started, animation: .spring())
-/// defer { send(.finished, animation: .default) }
-/// ```
-///
-/// See ``EffectPublisher/run(priority:operation:catch:file:fileID:line:)`` for more information on how to
-/// use this value to construct effects that can emit any number of times in an asynchronous
-/// context.
-///
-/// [callAsFunction]: https://docs.swift.org/swift-book/ReferenceManual/Declarations.html#ID622
-
 // MARK: - Composing Effects
-
-// MARK: - Testing Effects
 
 extension EffectPublisher {
   /// Merges a variadic list of effects together into a single effect, which runs the effects at the
@@ -604,56 +575,6 @@ extension EffectPublisher {
   }
 }
 
-// MARK: - Creating Effects
-
-/// A convenience type alias for referring to an effect that can never fail, like the kind of
-/// ``EffectPublisher`` returned by a reducer after processing an action.
-///
-/// Instead of specifying `Never` as `Failure`:
-///
-/// ```swift
-/// func reduce(into state: inout State, action: Action) -> EffectPublisher<Action, Never> { … }
-/// ```
-///
-/// You can specify a single generic:
-///
-/// ```swift
-/// func reduce(into state: inout State, action: Action) -> EffectTask<Action>  { … }
-/// ```
-
-/// A type that can send actions back into the system when used from
-/// ``EffectPublisher/run(priority:operation:catch:file:fileID:line:)``.
-///
-/// This type implements [`callAsFunction`][callAsFunction] so that you invoke it as a function
-/// rather than calling methods on it:
-///
-/// ```swift
-/// return .run { send in
-///   send(.started)
-///   defer { send(.finished) }
-///   for await event in self.events {
-///     send(.event(event))
-///   }
-/// }
-/// ```
-///
-/// You can also send actions with animation:
-///
-/// ```swift
-/// send(.started, animation: .spring())
-/// defer { send(.finished, animation: .default) }
-/// ```
-///
-/// See ``EffectPublisher/run(priority:operation:catch:file:fileID:line:)`` for more information on how to
-/// use this value to construct effects that can emit any number of times in an asynchronous
-/// context.
-///
-/// [callAsFunction]: https://docs.swift.org/swift-book/ReferenceManual/Declarations.html#ID622
-
-// MARK: - Composing Effects
-
-// MARK: - Testing Effects
-
 extension EffectPublisher {
   /// An effect that causes a test to fail if it runs.
   ///
@@ -776,56 +697,6 @@ extension EffectPublisher {
     }
   }
 }
-
-// MARK: - Creating Effects
-
-/// A convenience type alias for referring to an effect that can never fail, like the kind of
-/// ``EffectPublisher`` returned by a reducer after processing an action.
-///
-/// Instead of specifying `Never` as `Failure`:
-///
-/// ```swift
-/// func reduce(into state: inout State, action: Action) -> EffectPublisher<Action, Never> { … }
-/// ```
-///
-/// You can specify a single generic:
-///
-/// ```swift
-/// func reduce(into state: inout State, action: Action) -> EffectTask<Action>  { … }
-/// ```
-
-/// A type that can send actions back into the system when used from
-/// ``EffectPublisher/run(priority:operation:catch:file:fileID:line:)``.
-///
-/// This type implements [`callAsFunction`][callAsFunction] so that you invoke it as a function
-/// rather than calling methods on it:
-///
-/// ```swift
-/// return .run { send in
-///   send(.started)
-///   defer { send(.finished) }
-///   for await event in self.events {
-///     send(.event(event))
-///   }
-/// }
-/// ```
-///
-/// You can also send actions with animation:
-///
-/// ```swift
-/// send(.started, animation: .spring())
-/// defer { send(.finished, animation: .default) }
-/// ```
-///
-/// See ``EffectPublisher/run(priority:operation:catch:file:fileID:line:)`` for more information on how to
-/// use this value to construct effects that can emit any number of times in an asynchronous
-/// context.
-///
-/// [callAsFunction]: https://docs.swift.org/swift-book/ReferenceManual/Declarations.html#ID622
-
-// MARK: - Composing Effects
-
-// MARK: - Testing Effects
 
 @available(
   *,
