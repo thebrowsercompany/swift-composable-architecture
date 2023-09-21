5049bb730bdf7b41db04aeefae207ec000daa172
Brian Michel <brian.michel@gmail.com>
2023-09-21 18:54:07 -0400

    See what indentation looks like

--- Tests/ComposableArchitectureTests/EffectFailureTests.swift
+++ Tests/ComposableArchitectureTests/EffectFailureTests.swift
@@ -1,66 +1,66 @@
 #if DEBUG
-#if canImport(OpenCombine)
-import OpenCombine
-#else
-import Combine
-#endif
-import ComposableArchitecture
-import XCTest
+  #if canImport(OpenCombine)
+  import OpenCombine
+  #else
+  import Combine
+  #endif
+  import ComposableArchitecture
+  import XCTest
 
-@MainActor
-final class EffectFailureTests: XCTestCase {
-  var cancellables: Set<AnyCancellable> = []
+  @MainActor
+  final class EffectFailureTests: XCTestCase {
+    var cancellables: Set<AnyCancellable> = []
 
-  func testTaskUnexpectedThrows() async throws {
-    guard #available(iOS 15, macOS 12, tvOS 15, watchOS 8, *) else { return }
-    try XCTSkipIfWindowsExpectFailure()
+    func testTaskUnexpectedThrows() async throws {
+      guard #available(iOS 15, macOS 12, tvOS 15, watchOS 8, *) else { return }
+      try XCTSkipIfWindowsExpectFailure()
 
-    var line: UInt!
-    XCTExpectFailure {
-      $0.compactDescription
-        == """
-        An "EffectTask.task" returned from "\(#fileID):\(line+1)" threw an unhandled error. …
+      var line: UInt!
+      XCTExpectFailure {
+        $0.compactDescription
+          == """
+          An "EffectTask.task" returned from "\(#fileID):\(line+1)" threw an unhandled error. …
 
-            EffectFailureTests.Unexpected()
+              EffectFailureTests.Unexpected()
 
-        All non-cancellation errors must be explicitly handled via the "catch" parameter on \
-        "EffectTask.task", or via a "do" block.
-        """
+          All non-cancellation errors must be explicitly handled via the "catch" parameter on \
+          "EffectTask.task", or via a "do" block.
+          """
+      }
+
+      line = #line
+      let effect = EffectTask<Void>.task {
+        struct Unexpected: Error {}
+        throw Unexpected()
+      }
+
+      for await _ in effect.values {}
     }
 
-    line = #line
-    let effect = EffectTask<Void>.task {
-      struct Unexpected: Error {}
-      throw Unexpected()
-    }
+    func testRunUnexpectedThrows() async throws {
+      guard #available(iOS 15, macOS 12, tvOS 15, watchOS 8, *) else { return }
+      try XCTSkipIfWindowsExpectFailure()
 
-    for await _ in effect.values {}
+      var line: UInt!
+      XCTExpectFailure {
+        $0.compactDescription
+          == """
+          An "EffectTask.run" returned from "\(#fileID):\(line+1)" threw an unhandled error. …
+
+              EffectFailureTests.Unexpected()
+
+          All non-cancellation errors must be explicitly handled via the "catch" parameter on \
+          "EffectTask.run", or via a "do" block.
+          """
+      }
+
+      line = #line
+      let effect = EffectTask<Void>.run { _ in
+        struct Unexpected: Error {}
+        throw Unexpected()
+      }
+
+      for await _ in effect.values {}
+    }
   }
-
-  func testRunUnexpectedThrows() async throws {
-    guard #available(iOS 15, macOS 12, tvOS 15, watchOS 8, *) else { return }
-    try XCTSkipIfWindowsExpectFailure()
-
-    var line: UInt!
-    XCTExpectFailure {
-      $0.compactDescription
-        == """
-        An "EffectTask.run" returned from "\(#fileID):\(line+1)" threw an unhandled error. …
-
-            EffectFailureTests.Unexpected()
-
-        All non-cancellation errors must be explicitly handled via the "catch" parameter on \
-        "EffectTask.run", or via a "do" block.
-        """
-    }
-
-    line = #line
-    let effect = EffectTask<Void>.run { _ in
-      struct Unexpected: Error {}
-      throw Unexpected()
-    }
-
-    for await _ in effect.values {}
-  }
-}
 #endif
