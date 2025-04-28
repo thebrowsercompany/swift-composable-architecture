7bbf0749e643c9319ba2ee8ba278e25cd6ef38f0
Max Liberman <max.y.liberman@gmail.com>
2025-04-28 10:57:26 -0400

    Revert "Integrate instrumentation" (#57)
    This reverts commit fb6df4f6ed5b2ba5fb972b316d56a8856a1bf38e.

--- Sources/ComposableArchitecture/UIKit/IfLetUIKit.swift
+++ Sources/ComposableArchitecture/UIKit/IfLetUIKit.swift
@@ -51,9 +51,7 @@ extension Store {
   ///   goes from `nil` to non-`nil` and vice versa, so that the caller can react to these changes.
   public func ifLet<Wrapped>(
     then unwrap: @escaping (_ store: Store<Wrapped, Action>) -> Void,
-    else: @escaping () -> Void = {},
-    file: StaticString = #file,
-    line: UInt = #line
+    else: @escaping () -> Void = {}
   ) -> Cancellable where State == Wrapped? {
     return self.state
       .removeDuplicates(by: { ($0 != nil) == ($1 != nil) })
@@ -65,9 +63,7 @@ extension Store {
                 state = $0 ?? state
                 return state
               },
-              action: { $0 },
-              file: file,
-              line: line
+              action: { $0 }
             )
           )
         } else {
